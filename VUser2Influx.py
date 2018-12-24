853bd07cab059c4429a79718d90b5d067ffeb816
Erik van Oost <erik@eastern.nl>
2018-12-24 15:50:47 +0100

    Added support for multiple files handling, separate output to sqlite eg

--- VUser2Influx.py
+++ VUser2Influx.py
@@ -10,43 +10,27 @@ import sys
 from pytz import timezone
 from influxdb import InfluxDBClient
 from datetime import timedelta
-import transactions as x
+import transactions as transclass
 
-##
-## Check if data type of field is float
-##
-def isfloat(value):
-        try:
-            float(value)
-            return True
-        except:
-            return False
 
-##
-## Check if data type of field is int
-##
-def isinteger(value):
-        try:
-            if(float(value).is_integer()):
-                return True
-            else:
-                return False
-        except:
-            return False
 
 
 # parse the arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--batchsize', default=20000, help='How many inserts into InfluxDb in one chunk')
-parser.add_argument('--filename', help='VUser logfile to process', required=True)
+
+parser.add_argument('--filename', help='VUser logfile to process - wildcard possible', required=True)
+parser.add_argument('--csv', help='CSV file to write output to')
+parser.add_argument('--sqlite', help='SQLite file to write output to')
 parser.add_argument('--dbhost', default="localhost", help='InfluxDb hostname or ip')
 parser.add_argument('--dbport', default="8086", help='InfluxDb port number')
-parser.add_argument('--dbname', default="transactions", help='InfluxDb database name')
+parser.add_argument('--dbname', help='InfluxDb database name')
 parser.add_argument('--dbdrop', default=0, help='Drop database if exist to ensure a clean database')
+parser.add_argument('--batchsize', default=20000, help='How many inserts into InfluxDb in one chunk')
 parser.add_argument('--verbose', default=0, help='Display all parameters used in the script')
 
 args = parser.parse_args()
 
+# Show arguments if verbose
 if (args.verbose):    
     print("Filename=%s" %(args.filename))
     print("Batchsize=%d" %(args.batchsize))
@@ -57,193 +41,30 @@ if (args.verbose):
     print("JSON=%d" %(args.json))
 
 # 
-datapoints = []
+#datapoints = []
 transactions = []
 
 
-# connect to influx
-client = InfluxDBClient(host=args.dbhost, port=args.dbport)
-if (args.dbdrop):
-    client.drop_database(args.dbname)
+# open file and read content in to memory - automatically selects what the type of file is
+transactions.extend(transclass.transactiontype.ImportLogs(transactions,args.filename))
+#transactions.extend(transclass.transactiontype.VUserLog(args.filename))
+#transactions.extend(transclass.transactiontype.TruwebLog(args.filename))
 
-client.create_database(args.dbname)
-client.switch_database(args.dbname)
+# little status update
+print ("Found %d transactions " % (len(transactions)))
 
-# open file and read content in to memory
-transactions.extend(x.transactiontype.VUserLog(args.filename))
+if (args.dbname):
+    # export to Ifflux
+    transclass.transactiontype.Send2Influx(transactions, args.dbhost, args.dbport, args.dbname, args.batchsize, args.dbdrop)
 
-x.transactiontype.Send2SQLite(transactions, "testfile.db3", dropdatabase = 1)
+if (args.sqlite):
+    # export to SQLite
+    transclass.transactiontype.Send2SQLite(transactions, args.sqlite, dropdatabase = args.dbdrop)
 
-                    
-# all transactions in memory and export to desired output format
-#for transaction in transactions:
- #   print ("Influx: %s" % (transaction.InfluxDbLine()))
-  #  print ("SQLite: %s" % (transaction.SqLite()))
-
-
-#print (transactions[0].count)
-       
+if (args.csv):
+    # export to CSV
+    transclass.transactiontype.Send2CSV(transactions, args.csv)
+                   
 
 
 
-
-
-
-sys.exit()
-
-#reader = csv.reader(file)
-# set headers from the first row
-headers = next(reader, None)
-
-# preparete to build an in-memory table
-columns = {}
-for h in headers:
-    columns[h] = []
-
-# add all the values below the header
-for row in reader:
-    for h, v in zip(headers, row):
-       columns[h].append(v) 
-# dataset is now complete
-
-
-
-
-# walk through the dataset, column by column
-for column in columns:    
-    #print (column)
-
-    # extract the fields from the column name. With perfmon a header is like:
-    #
-    #\\APPSRV-SENSIGHT\Processor(_Total)\% Idle Time
-    #
-    #\\$host\$measurement($instance)\$field
-    # 
-    #
-    #  First replace the percentage % symbol with 'percent'
-    # TODO    
-    match = re.search(r'\\\\([a-zA-Z0-9-]+)\\([a-zA-Z0-9-_ \/\[\]%]+)(?:\(([a-zA-Z0-9-_\*#:\{\}\-\. \/\\?[\]%]+)\))?\\([a-zA-Z0-9-\(\)_ \/\[\]%]+)', column)
-
-    # start only inserting if matched, time column will be handled in the else
-    if match:
-
-        host = (match.group(1))
-
-        measurement = (match.group(2))
-        measurement = measurement.replace("Memory","win_mem")
-        measurement = measurement.replace("Network Interface","win_net")
-        measurement = measurement.replace("Paging File","win_swap")
-        measurement = measurement.replace("LogicalDisk","win_disk")
-        measurement = measurement.replace("PhysicalDisk","win_diskio")
-        measurement = measurement.replace("Processor","win_cpu")
-        measurement = measurement.replace("System","win_system")
-        measurement = measurement.replace("Process","win_proc")
-        
-
-        objectname = (match.group(2))
-        
-        instance = (match.group(3))
-        if not instance:
-            instance = ""
-        else:
-            instance = instance.replace("%", "Percent")
-            instance = instance.replace("/", "_")
-            instance = instance.replace(" ", "_")
-        
-        field = (match.group(4))
-        field = field.replace("%","Percent")
-        field = field.replace("/","_")
-        field = field.replace(" ","_")
-        field = field.replace("_sec","_persec")
-
-        print ('HOST %s WITH MEASUREMENT %s FOR INSTANCE %s WITH FIELD %s' %(host, measurement, instance, field,))
-
-        # combine the value with the timestamp
-        for value,timestamp in zip(columns[column], columns[headers[0]]):
-            #print (value)
-            # build the json object
-            if (isfloat(value) or isinteger(value)):
-                if (args.json):
-
-                    datapoint =             {
-                                "measurement": measurement,
-                                "tags": {
-                                    "host": host,
-                                    "instance": instance,
-                                    "objectname" : objectname
-                                },
-                                "time":  timestamp,
-                                "fields": {
-                                    field: float(value  )
-                                }
-                            }
-                    #print(value)
-                    #print (datapoint)
-                    datapoints.append(datapoint)
-
-                    #client.write_points(json_body)
-
-                    if len(datapoints) % args.batchsize == 0:
-                        #print('Read %d lines'%count)
-                        print('Inserting %d datapoints...'%(len(datapoints)))
-                        response = client.write_points(datapoints)
-
-                        if response == False:
-                            print('Problem inserting points, exiting...')
-                            exit(1)
-
-                        print("Wrote %d, response: %s" % (len(datapoints), response))
-
-
-                        datapoints = []            
-                else:
-                    datapoint = "%s host=%s, instance=%s, objectname=%s %s=%d %d" %(measurement, host, instance, objectname, field, float(value), timestamp )
-                    #client.write(datapoint)
-                    #datapoints.append(datapoint)
-
-                    print(datapoint)
-    else:
-        # this it the time column, convert the timestamps from 12/24/2016 14:57:13.810 to 2018-03-28T8:01:00Z"
-        print ("Asuming %s is the time column" % (column))
-        
-        
-        match = re.search(r'\((.*?)\)\s?\((.*?)\)\((.*?)\)', column)
-
-        importtype = (match.group(1))
-        timezone = (match.group(2))
-        correction = float(match.group(3))
-
-        print ("")
-        print ("Correction: %d" %(correction))
-        print ("Timezone: %s" %(timezone))        
-
-        if (args.json):
-            columns[column] = [
-                (datetime.datetime.strptime(timestamp,"%m/%d/%Y %H:%M:%S.%f") + timedelta(minutes=correction)).strftime("%Y-%m-%dT%H:%M:%SZ")
-                for timestamp in columns[column]
-                ]
-        else:
-            columns[column] = [
-                (datetime.datetime.strptime(timestamp,"%m/%d/%Y %H:%M:%S.%f") + timedelta(minutes=correction)).timestamp()
-                for timestamp in columns[column]
-                ]
-
-
-        #print ("after")
-
-        #for timestamp in columns[column]:
-        #    print (timestamp)            
-
-
- # write rest
-if len(datapoints) > 0:        
-    print('Inserting last %d datapoints...'%(len(datapoints)))
-    response = client.write_points(datapoints)
-
-    if response == False:
-        print('Problem inserting points, exiting...')
-        exit(1)
-
-    print("Wrote %d, response: %s" % (len(datapoints), response))           
-
-print("end")
